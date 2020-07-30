900168143194e6d198901a9414cc8e773a733c4b
Mike Desaro <mike@prime31.com>
2020-07-30 13:36:09 -0700

    fix tests

--- src/tests.zig
+++ src/tests.zig
@@ -1,9 +1,6 @@
-
 // include all files with tests
 comptime {
     _ = @import("fs.zig");
-    _ = @import("mem/sdl_allocator.zig");
+    _ = @import("mem/allocator.zig");
     _ = @import("mem/scratch_allocator.zig");
-    _ = @import("mem/sdl_stream.zig");
-
 }
