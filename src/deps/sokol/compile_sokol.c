e0ace1bbed5cb2098c5fc4dc233e198f7307a6b7
Mike Desaro <mike@prime31.com>
2020-07-30 17:38:04 -0700

    temporary hack for windows

--- src/deps/sokol/compile_sokol.c
+++ src/deps/sokol/compile_sokol.c
@@ -15,3 +15,12 @@
 #include "cimgui/cimgui.h"
 #define SOKOL_IMGUI_IMPL
 #include "sokol/util/sokol_imgui.h"
+
+// this is a monstrous hack until I can figure out why zig is looking for a WinMain...
+#if defined(_WIN32)
+int main(void);
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+    main();
+    return 0;
+}
+#endif
