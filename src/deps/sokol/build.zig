0cddbfb959fd7b03093ae68a4afba82f23a334b0
Mike <mike@prime31.com>
2021-01-02 15:14:08 -0800

    keeping this hack out until 0.7.1+ works on arm mac

--- src/deps/sokol/build.zig
+++ src/deps/sokol/build.zig
@@ -28,12 +28,12 @@ pub fn linkArtifact(b: *Builder, exe: *std.build.LibExeObjStep, target: std.buil
     exe.addCSourceFile(prefix_path ++ "src/deps/sokol/compile_sokol.c", &c_flags);
 }
 
-/// macOS helper function to add SDK search paths 
+/// macOS helper function to add SDK search paths
 fn addMacosSdkDirs(b: *Builder, step: *std.build.LibExeObjStep) !void {
     const sdk_dir = try std.zig.system.getSDKPath(b.allocator);
     const framework_dir = try std.mem.concat(b.allocator, u8, &[_][]const u8 { sdk_dir, "/System/Library/Frameworks" });
-    const usrinclude_dir = try std.mem.concat(b.allocator, u8, &[_][]const u8 { sdk_dir, "/usr/include"});
+    // const usrinclude_dir = try std.mem.concat(b.allocator, u8, &[_][]const u8 { sdk_dir, "/usr/include"});
     step.addFrameworkDir(framework_dir);
-    step.addIncludeDir(usrinclude_dir);
+    // step.addIncludeDir(usrinclude_dir);
 }
 
