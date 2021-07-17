0e58ee0b3e0351183a3c7839e59867df59a7a6fd
prime31 <mike@prime31.com>
2021-07-17 13:11:04 -0700

    Adios buffer stream

--- src/mem/mem.zig
+++ src/mem/mem.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
 const ScratchAllocator = @import("scratch_allocator.zig").ScratchAllocator;
 
-pub const BufferStream = @import("stream.zig").BufferStream;
-
 // temp allocator is a ring buffer so memory doesnt need to be freed
 pub var tmp_allocator: *std.mem.Allocator = undefined;
 var tmp_allocator_instance: ScratchAllocator = undefined;
